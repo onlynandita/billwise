dbf9c5fbc558823ef046ad6056c9f3623f040e3f
onlynandita <nanditadad331@gmail.com>
2026-07-16 22:16:58 +0530

    Add files via upload

---
+++ projects/billwise/src/test/java/com/apdcl/billwise/BillwiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apdcl.billwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillwiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
